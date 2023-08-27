87ba097135a5704972823f982b2c2f39aac0a9fb
Himanshu Walia <himanshu-walia@cookpad.jp>
2023-08-27 15:01:29 +0100

    implement vm with the UI model mapper

---
+++ feature-albums/src/main/java/com/nutmeg/android/feature/albums/AlbumDetailUIModelMapper.kt
@@ -0,0 +1,30 @@
+package com.nutmeg.android.feature.albums
+
+import androidx.paging.PagingData
+import androidx.paging.map
+import com.nutmeg.android.feature.albums.model.AlbumDetailUIModel
+import com.nutmeg.android.model.AlbumDetail
+import com.nutmeg.android.model.User
+import dagger.hilt.android.scopes.ViewModelScoped
+import javax.inject.Inject
+
+@ViewModelScoped
+class AlbumDetailUIModelMapper @Inject constructor() {
+
+    fun map(
+        pagingData: PagingData<AlbumDetail>,
+        users: List<User>
+    ): PagingData<AlbumDetailUIModel> {
+
+        return pagingData.map { detail ->
+            val album = detail.album
+            AlbumDetailUIModel(
+                albumId = album.id,
+                albumTitle = album.title,
+                userName = users.find { album.userId == it.id }?.name ?: "Unknown",
+                photoTitle = detail.photos.photoTitle,
+                thumbnailUrl = detail.photos.thumbnailUrl
+            )
+        }
+    }
+}
---
+++ feature-albums/src/main/java/com/nutmeg/android/feature/albums/model/AlbumDetailUIModel.kt
@@ -0,0 +1,9 @@
+package com.nutmeg.android.feature.albums.model
+
+data class AlbumDetailUIModel(
+    val albumId: Int,
+    val albumTitle: String,
+    val userName: String,
+    val photoTitle: String,
+    val thumbnailUrl: String
+)
